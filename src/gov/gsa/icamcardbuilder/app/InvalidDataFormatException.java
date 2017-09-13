794456c7bb68422acfc1dc17e9b946b9aac941a7
Bob Fontana <bob.fontana@certipath.com>
2017-09-12 21:01:29 -0400

    Added InvalidDataFormatException class.

---
+++ src/gov/gsa/icamcardbuilder/app/InvalidDataFormatException.java
@@ -0,0 +1,22 @@
+package gov.gsa.icamcardbuilder.app;
+import org.apache.logging.log4j.LogManager;
+
+import static gov.gsa.icamcardbuilder.app.Gui.dateFormat;
+import static gov.gsa.icamcardbuilder.app.Gui.logger;
+
+import java.util.Date;
+
+@SuppressWarnings("serial")
+public class InvalidDataFormatException extends Exception {
+
+	private String message = null;
+	
+	public InvalidDataFormatException (String errorMessage, String className) {
+		super();
+		logger = LogManager.getLogger(className);
+		this.message = errorMessage;
+		logger.error(message);
+		Gui.status.append(dateFormat.format(new Date()) + " - " + message + "\n");
+		Gui.errors = true;
+	}
+}
