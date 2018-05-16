e584f85e3beb844482fe94efb8f9f9c77c005c4e
Bob Fontana <bob.fontana@certipath.com>
2018-05-16 15:08:43 -0400

    Created altered objects and scripts for Cards 6, 7, 8

---
+++ cards/ICAM_Card_Objects/07_Tampered_Fingerprints/altercbeff.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cp -p '9 - Fingerprints' finger.bin
+SIZE=$(stat --format="%s" finger.bin)
+dd if=finger.bin bs=1 count=88 of=p1 2>/dev/null
+echo -e -n "\\xff\\xff\\xff\\xff" >p2
+dd if=finger.bin bs=1 skip=92 of=p3 2>/dev/null
+cat p1 p2 p3 >p4
+mv p4 '9 - Fingerprints'
+rm -f p* finger.bin
---
+++ cards/ICAM_Card_Objects/08_Tampered_Security_Object/alterso.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cp -p '2 - Security Object' so.bin
+SIZE=$(stat --format="%s" so.bin)
+OFFSET1=$(expr $SIZE - 16)
+OFFSET2=$(expr $OFFSET1 + 4)
+dd if=so.bin bs=1 count=$OFFSET1 of=p1 2>/dev/null
+echo -e -n "\\xff\\xff\\xff\\xff" >p2
+dd if=so.bin bs=1 skip=$OFFSET2 of=p3 2>/dev/null
+cat p1 p2 p3 >p4
+mv p4 '2 - Security Object'
+rm -f p* so.bin
