3b1bacaf88623cf3a24143120ba3cb7a363cf7db
Bob Fontana <bob.fontana@certipath.com>
2018-07-24 13:53:56 -0400

    Re-signed content

--- cards/ICAM_Card_Objects/06_Tampered_PHOTO/alterphoto.sh
+++ cards/ICAM_Card_Objects/06_Tampered_PHOTO/alterphoto.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-cp -p '10 - Face Object' face.bin
+cp -p '10 - Face Object Untampered' face.bin
 SIZE=$(stat --format="%s" face.bin)
 dd if=face.bin bs=1 count=2430 of=p1 2>/dev/null
 echo -e -n "\\xff\\xfe\\xfd\\xfb" >p2
 dd if=face.bin bs=1 skip=2434 of=p3 2>/dev/null
 cat p1 p2 p3 >p4
-mv p4 '10 - Face Object'
+mv p4 '10 - Face Object' # Tampered
 rm -f p* face.bin
+cp -p ../05_Tampered_Certificates/'10 - Face Object' '10 - Face Object Untampered'
--- cards/ICAM_Card_Objects/07_Tampered_Fingerprints/alterfps.sh
+++ cards/ICAM_Card_Objects/07_Tampered_Fingerprints/alterfps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cp -p '9 - Fingerprints' finger.bin
+cp -p '9 - Fingerprints Untampered' finger.bin
 SIZE=$(stat --format="%s" finger.bin)
 dd if=finger.bin bs=1 count=500 of=p1 2>/dev/null
 echo -e -n "\\xff\\xff\\xff\\xff" >p2
@@ -7,3 +7,4 @@ dd if=finger.bin bs=1 skip=504 of=p3 2>/dev/null
 cat p1 p2 p3 >p4
 mv p4 '9 - Fingerprints'
 rm -f p* finger.bin
+cp -p ../06_Tampered_PHOTO/'9 - Fingerprints' '9 - Fingerprints Untampered'
