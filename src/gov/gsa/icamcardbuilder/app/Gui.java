05eb965dea0c98a105b2e05d7baaa68a60d9a10f
Bob Fontana <bob.fontana@certipath.com>
2018-07-26 11:57:44 -0400

    Updated to v1.8.61

--- src/gov/gsa/icamcardbuilder/app/Gui.java
+++ src/gov/gsa/icamcardbuilder/app/Gui.java
@@ -70,7 +70,7 @@ import javax.swing.JCheckBox;
 public class Gui extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	protected final static String version = "1.8.60";
+	protected final static String version = "1.8.61";
 	protected static String cardsDirectory = null;
 	private static String cardsDirectoryArg = null;
 	protected static boolean debug = true;
