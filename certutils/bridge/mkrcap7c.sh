c8e29925eca26c406b09855c92c6d0731fcf4b65
bob-fontana <bob.fontana@certipath.com>
2018-10-30 15:48:04 -0400

    Removed from the repo

--- certutils/bridge/mkrcap7c.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-# Create the command to populate the certsIssuedToICAMTestCardRootCA.p7c file like this:
-#
-openssl crl2pkcs7 -nocrl -outform der -out ../aia/certsIssuedToICAMTestCardRootCA.p7c -certfile 99_ICAM_Test_Card_Bridge_CA_to_ICAM_Test_Card_Root_CA.crt
