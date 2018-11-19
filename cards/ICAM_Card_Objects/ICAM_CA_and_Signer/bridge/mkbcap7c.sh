4bbf6ebd533dd6d810d609acf26bdb609524eed3
bob-fontana <bob.fontana@certipath.com>
2018-11-19 12:18:23 -0500

    Regenerate .p7c files

--- cards/ICAM_Card_Objects/ICAM_CA_and_Signer/bridge/mkbcap7c.sh
+++ cards/ICAM_Card_Objects/ICAM_CA_and_Signer/bridge/mkbcap7c.sh
@@ -3,6 +3,15 @@
 # Create the command to populate the certsIssuedToICAMTestCardBridgeCA.p7c file like this:
 #
 
+# Make separate .p7c files
+
+for F in $(ls *.crt); do
+  AIA=$(openssl x509 -in $F -text -noout | grep /aia/ | sed s'!^.*aia/!!g')
+  if [ z$AIA != "z" ]; then
+    openssl crl2pkcs7 -certfile $F -nocrl -outform der -out ../aia/$AIA
+  fi
+done
+
 echo "rm -f Intermediate_Bridge_Certs.p7b" >/tmp/t.$$.sh
 ls [0-3]*.crt ICAM_Test_Card_Bridge_CA.crt | awk 'BEGIN { printf ("openssl crl2pkcs7 -nocrl -outform der -out ../aia/certsIssuedToICAMTestCardBridgeCA.p7c ") } { printf("-certfile %s ", $1) }' >>/tmp/t.$$.sh
 echo >>/tmp/t.$$.sh
