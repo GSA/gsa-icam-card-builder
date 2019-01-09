f2cf537ee327de088dc13c2691dd0169058f2d55
bob-fontana <bob.fontana@certipath.com>
2019-01-09 09:40:40 -0500

    Replaced symlink with cp

--- cards/ICAM_Card_Objects/ICAM_CA_and_Signer/bridge/mkbcap7c.sh
+++ cards/ICAM_Card_Objects/ICAM_CA_and_Signer/bridge/mkbcap7c.sh
@@ -16,7 +16,7 @@ echo "rm -f Intermediate_Bridge_Certs.p7b" >/tmp/t.$$.sh
 ls [0-3]*.crt ICAM_Test_Card_Bridge_CA.crt | awk 'BEGIN { printf ("openssl crl2pkcs7 -nocrl -outform der -out ../aia/certsIssuedToICAMTestCardBridgeCA.p7c ") } { printf("-certfile %s ", $1) }' >>/tmp/t.$$.sh
 echo >>/tmp/t.$$.sh
 
-echo "ln -s ../aia/certsIssuedToICAMTestCardBridgeCA.p7c Intermediate_Bridge_Certs.p7b" >>/tmp/t.$$.sh
+echo "cp -p ../aia/certsIssuedToICAMTestCardBridgeCA.p7c Intermediate_Bridge_Certs.p7b" >>/tmp/t.$$.sh
 
 #
 # Create the command to populate the certsIssuedToICAMTestCardRootCA.p7c file like this:
